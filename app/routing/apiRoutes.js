97bfb36a84078bf1b28b96c234206c17bb451c60
Jr <jgrantlyons@gmail.com>
2019-01-24 12:02:05 -0800

    compatability issues

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -20,11 +20,58 @@ module.exports = function (app) {
     // This will be used to handle incoming survey results. 
     // This route will also be used to handle the compatibility logic.
     app.post("/api/friends", function (req, res) {
-        // console.log(req.body);
-        console.log(req.body);
+
+        let results = [];
+        
+        let oldFriendScores = req.body.scores;
+        // console.log("string array", oldFriendScores);
+        let friendScores = oldFriendScores.map(function (arrayElement){
+            return parseInt(arrayElement);
+        });
+        req.body.scores = friendScores;
         friends.push(req.body);
 
-        return res.json(req.body);
+        // console.log("int array: ", friendScores);
+        // // return newFriendScores;
+        
+        let allProfiles = friends.profiles;
+        for(let i = 0; i>allProfiles.length - 1; i++){
+            let friendArr = allProfiles[i].scores;
+            let difference;
+            for(let j = 0; j<friendArr.length; j++){
+                let dif = Math.abs(friendArr[j]) - friendScores[j];
+                difference = dif; 
+            };
+
+            let result = {
+                name: allProfiles[i].name,
+                photo: allProfiles[i].pictureLink,
+                scoreDifference: difference
+            };
+
+            results.push(result);
+            // for(let j = 0; j>allProfiles)
+
+        };
+
+        let match = results.reduce(function(previous, current){
+            return previous.scoreDifference < current.scoreDifference ? previous : current;
+
+        });
+
+        res.json(match);
+
+        
+        // console.log(req.body);
+        // console.log(req.body);
+        // friends.push(req.body);
+
+        // console.log(parseInt(req.body.scores))
+        // console.log(req.body.scores);
+        // let comparee = req.body;
+
+
+        // return res.json(req.body);
     });
 };
 
